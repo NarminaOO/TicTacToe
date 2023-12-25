ec9a646fc34d2d87eb144fb0f8bdf11fb024bc13
Narmina <149943881+NarminaOO@users.noreply.github.com>
2023-12-25 18:30:40 +0400

    add functions 2
    add checkForWinner(); add newGame()

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -37,4 +37,102 @@ void ticTacToe::print()
         std::cout << "\n";
     }
 }
+int ticTacToe::checkForWinner() 
+{
+    int count1{}, count2{};
+
+    if (turn == n * n) 
+    {
+        gameEnded = true;
+    }
+
+    for (size_t i = 0; i < n; ++i) 
+    {
+        count1 = 0;
+        count2 = 0;
+
+        for (size_t j = 0; j < n; ++j) 
+        {
+            if (count1 != 0 && count2 != 0)
+                break;
+
+            if ((*grid)[i][j] == 1)
+                count1++;
+            else if ((*grid)[i][j] == 2)
+                count2++;
+        }
+        if (count1 == n)
+            return 1;
+        if (count2 == n)
+            return 2;
+    }
+
+    for (size_t i = 0; i < n; ++i) 
+    {
+        count1 = 0;
+        count2 = 0;
+
+        for (size_t j = 0; j < n; ++j) 
+        {
+            if (count1 != 0 && count2 != 0)
+                break;
+
+            if ((*grid)[j][i] == 1)
+                count1++;
+            else if ((*grid)[j][i] == 2)
+                count2++;
+        }
+        if (count1 == n)
+            return 1;
+        if (count2 == n)
+            return 2;
+    }
+
+    count1 = 0;
+    count2 = 0;
+
+    for (size_t i = 0; i < n; ++i) 
+    {
+        if (count1 != 0 && count2 != 0)
+            break;
+        if ((*grid)[i][i] == 1)
+            count1++;
+        else if ((*grid)[i][i] == 2)
+            count2++;
+    }
+
+    if (count1 == n)
+        return 1;
+    if (count2 == n)
+        return 2;
+
+    count1 = 0;
+    count2 = 0;
+
+    for (size_t i = 0; i < n; ++i) 
+    {
+        if (count1 != 0 && count2 != 0)
+            break;
+        if ((*grid)[n - i - 1][i] == 1)
+            count1++;
+        else if ((*grid)[n - i - 1][i] == 2)
+            count2++;
+    }
+
+    if (count1 == n)
+        return 1;
+    if (count2 == n)
+        return 2;
+
+    return 0;
+}
+
+void ticTacToe::newGame() 
+{
+    grid->clear();
+    grid = std::make_unique<std::vector<std::vector<int>>>(n, std::vector<int>(n, 0));
+    gameEnded = false;
+    turn = 0;
+    prevPlayed = 0;
+}
 
