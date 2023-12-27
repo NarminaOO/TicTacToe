080e3ddfcd85d7e7402cc807cfc36575d3b96a1d
Narmina <149943881+NarminaOO@users.noreply.github.com>
2023-12-27 13:16:59 +0400

    move template functions into header file

--- ticTacToeClass.cpp
+++ ticTacToeClass.cpp
@@ -4,23 +4,6 @@
 #include <type_traits>
 #include "ticTacToeClass.h"
 
-template <typename onlyInt>
-ticTacToe::ticTacToe(onlyInt n) : n(n), turn(0), gameEnded(false), prevPlayed(0) 
-{
-    static_assert(std::is_same<onlyInt, int>::value, "input must be integer");
-
-    try 
-    {
-        grid = std::make_unique<std::vector<std::vector<int>>>(n, std::vector<int>(n, 0));
-    }
-    catch (const std::bad_alloc& ex) 
-    {
-        std::cout << "Error:" << ex.what() << "\n";
-        std::cout << "Deleting object:" << "\n";
-        grid.reset(nullptr);
-    }
-}
-
 void ticTacToe::print() 
 {
     for (auto iter1 = grid->begin(); iter1 != grid->end(); ++iter1) 
@@ -136,69 +119,4 @@ void ticTacToe::newGame()
     prevPlayed = 0;
 }
 
-template <typename onlyInt>
-int ticTacToe::Move(onlyInt col, onlyInt row, onlyInt player) {
-    static_assert(std::is_same<onlyInt, int>::value, "input must be integer");
 
-    if (gameEnded) {
-        std::cout << "Game is over\n"
-            << "Start a new game?\ny/n?\n";
-
-        char choice{};
-        std::cin >> choice;
-
-        switch (choice) {
-        case 'y': {
-            newGame();
-            return -1;
-        }
-        case 'n': {
-            return -1;
-        }
-        default:
-            return -1;
-        }
-    }
-
-    if (player != 1 && player != 2) {
-        std::cout << "Only 2 players are allowed\n";
-        return -1;
-    }
-
-    if (prevPlayed == player) {
-        std::cout << "Player " << player << " made their turn twice in a row\n";
-        return -1;
-    }
-    
-    try {
-        if ((*grid).at(row).at(col) != 0) {
-            std::cout << "This square is already occupied\n";
-            return -1;
-        }
-
-        if (player == 1)
-            (*grid)[row][col] = player;
-        else if (player == 2)
-            (*grid)[row][col] = player;
-    }
-    catch (const std::out_of_range& exc) {
-        std::cout << "Error: " << exc.what() << "\n";
-        return -1;
-    }
-
-    turn++;
-    int win = checkForWinner();
-    if (win == 0)
-        return 0;
-    if (win == 1) {
-        gameEnded = true;
-        return 1;
-    }
-
-    if (win == 2) {
-        gameEnded = true;
-        return 2;
-    }
-
-    return 0;
-}
